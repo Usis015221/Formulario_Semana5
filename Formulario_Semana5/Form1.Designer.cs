aea08f8ab9f8afed95de7778791856ed27dfb74d
GEOVANNI ALEXANDER GUERRERO MUNGUIA <USIS015221@ugb.edu.sv>
2021-08-28 22:46:34 -0600

    Agregar archivos de proyecto.

---
+++ Formulario_Semana5/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace Formulario_Semana5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importarExportarProyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarEnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otrosProyectosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.antiguosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Arial", 13F);
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.otrosProyectosToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1176, 39);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.nuevoProyectoToolStripMenuItem,
+            this.importarExportarProyectoToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(116, 35);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(425, 40);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // nuevoProyectoToolStripMenuItem
+            // 
+            this.nuevoProyectoToolStripMenuItem.Name = "nuevoProyectoToolStripMenuItem";
+            this.nuevoProyectoToolStripMenuItem.Size = new System.Drawing.Size(425, 40);
+            this.nuevoProyectoToolStripMenuItem.Text = "Nuevo Proyecto";
+            // 
+            // importarExportarProyectoToolStripMenuItem
+            // 
+            this.importarExportarProyectoToolStripMenuItem.Name = "importarExportarProyectoToolStripMenuItem";
+            this.importarExportarProyectoToolStripMenuItem.Size = new System.Drawing.Size(425, 40);
+            this.importarExportarProyectoToolStripMenuItem.Text = "Importar/Exportar Proyecto";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.guardarComoToolStripMenuItem,
+            this.guardarEnToolStripMenuItem});
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(125, 35);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(297, 40);
+            this.guardarComoToolStripMenuItem.Text = "Guardar como..";
+            // 
+            // guardarEnToolStripMenuItem
+            // 
+            this.guardarEnToolStripMenuItem.Name = "guardarEnToolStripMenuItem";
+            this.guardarEnToolStripMenuItem.Size = new System.Drawing.Size(297, 40);
+            this.guardarEnToolStripMenuItem.Text = "Guardar en...";
+            // 
+            // otrosProyectosToolStripMenuItem
+            // 
+            this.otrosProyectosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.recientesToolStripMenuItem,
+            this.actualToolStripMenuItem,
+            this.antiguosToolStripMenuItem});
+            this.otrosProyectosToolStripMenuItem.Name = "otrosProyectosToolStripMenuItem";
+            this.otrosProyectosToolStripMenuItem.Size = new System.Drawing.Size(213, 35);
+            this.otrosProyectosToolStripMenuItem.Text = "Otros Proyectos";
+            // 
+            // recientesToolStripMenuItem
+            // 
+            this.recientesToolStripMenuItem.Name = "recientesToolStripMenuItem";
+            this.recientesToolStripMenuItem.Size = new System.Drawing.Size(232, 40);
+            this.recientesToolStripMenuItem.Text = "Actual ";
+            // 
+            // actualToolStripMenuItem
+            // 
+            this.actualToolStripMenuItem.Name = "actualToolStripMenuItem";
+            this.actualToolStripMenuItem.Size = new System.Drawing.Size(232, 40);
+            this.actualToolStripMenuItem.Text = "Recientes";
+            this.actualToolStripMenuItem.Click += new System.EventHandler(this.actualToolStripMenuItem_Click);
+            // 
+            // antiguosToolStripMenuItem
+            // 
+            this.antiguosToolStripMenuItem.Name = "antiguosToolStripMenuItem";
+            this.antiguosToolStripMenuItem.Size = new System.Drawing.Size(232, 40);
+            this.antiguosToolStripMenuItem.Text = "Antiguos";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(109, 35);
+            this.ayudaToolStripMenuItem.Text = "Ayuda ";
+            this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click);
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(150, 35);
+            this.acercaDeToolStripMenuItem.Text = "Acerca De";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(484, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 91);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Aceptar/Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(888, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(241, 91);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Si/No";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(67, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(241, 91);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Exclamación";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(484, 282);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(241, 91);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Interrogación";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(888, 282);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(241, 91);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Error";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1176, 709);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario Semana 5";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoProyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importarExportarProyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarEnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otrosProyectosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actualToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem antiguosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Formulario_Semana5/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario_Semana5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void actualToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has elegido la opcion Aceptar", "Opcion Aceptar", MessageBoxButtons.OK);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has elegido la opcion Aceptar/Cancelar", "Opcion Aceptar/Cancelar", MessageBoxButtons.OKCancel);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has elegido la opcion Si/No", "Opcion Si/No", MessageBoxButtons.YesNo);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has elegido la opcion Exclamacion", "Opcion Exclamacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has elegido la opcion Interrogacion", "Opcion Interrogacion", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Has elegido la opcion Error", "Opcion Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Ayuda f2 = new Ayuda();
+            f2.ShowDialog();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 f2 = new AboutBox1();
+            f2.ShowDialog();
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
